88a259278bb309d73bea9cd9283cd0431d820437
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-07-19 18:32:29 +0000

    Automatically backed up by Learn

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -5,12 +5,12 @@ export default function manageBand(state = {
     case 'ADD_BAND':
       const band = {
         id: Math.random()*10000000000000000,
-        name: action.payload
+        name: action.name
       }
       return { ...state, bands: [...state.bands, band] }
 
     case 'DELETE_BAND':
-      return {bands: state.bands.filter(band => band.id !== action.payload)}
+      return {bands: state.bands.filter(band => band.id !== action.id)}
 
     default:
       return state;
