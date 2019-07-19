88bf11e5454def0832b9eb19a66a5a0a2fada8bc
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-07-19 18:06:12 +0000

    Automatically backed up by Learn

--- src/components/Band.js
+++ src/components/Band.js
@@ -5,7 +5,7 @@ class Band extends Component {
   render() {
     return(
       <div>
-        Band Component
+        <span>{this.props.band.name}</span><button onClick={() => this.props.delete(this.props.band.id)}>DELETE</button>
       </div>
     );
   }
